8e0668b5097791ca103a5517453350e37d497d8b
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2024-06-02 10:50:24 +0300

    Update cat.asm

--- example/cat.asm
+++ example/cat.asm
@@ -1,11 +1,38 @@
+number:
+    word 1
+print_name:
+    pop
+    push_addr number
+    load
+    store 2
+    push_addr number
+    load
+    push 10
+    sub
+    jz end
+    push_addr number
+    inc
+    pop_addr number
+    jmp print_name
+end:
+    push 1
+    pop_addr number
+    ret
+num:
+    word 0
 _start:
-      ei
-      load_symbol
-      push 10
-      sub
-      jz print
-      jmp _start
-  print:
-      load
-      store
-      halt
+    ei
+    push_addr num
+    load
+    push 10
+    sub
+    di
+    jz print
+    pop
+    push_addr num
+    inc
+    pop_addr num
+    jmp _start
+print:
+    call print_name
+    halt
