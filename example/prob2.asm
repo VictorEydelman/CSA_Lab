34f0d18c38d64615ace6ca9cb7a6032dbc630dca
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2024-06-02 10:54:41 +0300

    Update prob2.asm

--- example/prob2.asm
+++ example/prob2.asm
@@ -1,44 +1,46 @@
-    num:
-        push_addr 101
+      max:
+        word 4000000
+    now:
+        word 0
+    sum:
+        word 2
+    prov:
+        push_addr now
         push 2
         div
         push 2
         mul
-        push_addr 101
+        push_addr now
         sub
         jz ch
         ret
     ch:
         pop
-        push_addr 101
-        push_addr 102
+        push_addr now
+        push_addr sum
         add
-        pop_addr 102
+        pop_addr sum
         push 0
         ret
     _start:
-        push 2
-        pop_addr 102
-        push 4000000
-        pop_addr 103
         push 2
         push 2
         push 1
     loop:
         add
-        pop_addr 101
-        push_addr 101
+        pop_addr now
+        push_addr now
         swap
-        push_addr 101
-        call num
+        push_addr now
+        call prov
         pop
-        push_addr 103
-        push_addr 101
+        push_addr max
+        push_addr now
         sub
         jn end
         pop
         jmp loop
     end:
-        push_addr 102
-        store
+        push_addr sum
+        store 1
         halt
