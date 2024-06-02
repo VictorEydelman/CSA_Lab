f291ae07a9ca0a0234978a5f6c2c533f304a789a
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2024-06-02 15:51:05 +0300

    Update isa.py

--- isa.py
+++ isa.py
@@ -3,34 +3,34 @@ from enum import Enum
 
 
 class Opcode(str, Enum):
-    INC = "inc"  # 0
-    DEC = "dec"  # 0
-    NOP = "nop"  # 0
-    HALT = "halt"  # 0
-    EI = "ei"  # 0
-    DI = "di"  # 0
-    PUSH = "push"  # 1
+    INC = "inc"
+    DEC = "dec"
+    NOP = "nop"
+    HALT = "halt"
+    EI = "ei"
+    DI = "di"
+    PUSH = "push"
     PUSH_ADDR = "push_addr"
     PUSH_BY = "push_by"
-    POP = "pop"  # 0
+    POP = "pop"
     POP_ADDR = "pop_addr"
-    JZ = "jz"  # 2
-    JNZ = "jnz"  # 2
-    JN = "jn"  # 2
-    JNS = "jns"  # 2
-    JMP = "jmp"  # 2
-    ADD = "add"  # 0
-    SUB = "sub"  # 0
-    MUL = "mul"  # 0
-    DIV = "div"  # 0
-    CALL = "call"  # 2
-    RET = "ret"  # 0
-    WORD = "word"  # 1
-    RESW = "resw"  # 1
-    LOAD = "load"  # 2
-    SWAP = "swap"  # 0
+    JZ = "jz"
+    JNZ = "jnz"
+    JN = "jn"
+    JNS = "jns"
+    JMP = "jmp"
+    ADD = "add"
+    SUB = "sub"
+    MUL = "mul"
+    DIV = "div"
+    CALL = "call"
+    RET = "ret"
+    WORD = "word"
+    RESW = "resw"
+    LOAD = "load"
+    SWAP = "swap"
     STORE = "store"
-    LOAD_SYMBOL = "load_symbol"
+    DUP = "dup"
 
     def __str__(self):
         return self.value
